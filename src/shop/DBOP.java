5c87b4cf5df73447839b233ae96339f21388eae0
Amila95 <amilawicramarathna95@gmail.com>
2017-12-24 22:03:22 +0530

    update

--- src/shop/DBOP.java
+++ src/shop/DBOP.java
@@ -18,11 +18,11 @@ public class DBOP {
     void seachitem(Item I){
     try{
         Statement s = Database.getStatement();
-        String pqr = ("SELECT * FROM pricelist WHERE ID ='"+I.getItemID()+"'");
+        String pqr = ("SELECT * FROM item WHERE item_id ='"+I.getItemID()+"'");
         ResultSet rs = s.executeQuery(pqr);
         while(rs.next()){
-            I.setName(rs.getString("Name"));
-            I.setPrice(Integer.parseInt(rs.getString("Price")));
+            I.setName(rs.getString("item_name"));
+            I.setPrice(Integer.parseInt(rs.getString("item_price")));
         }
     }
     catch(Exception ex){
@@ -32,7 +32,7 @@ public class DBOP {
     void settotalcount(Item I){
     try{
         Statement s = Database.getStatement();
-        String pq = ("SELECT Count FROM pricelist WHERE ID ='"+I.getItemID()+"'");
+        String pq = ("SELECT item_quantity FROM item WHERE item_id ='"+I.getItemID()+"'");
         ResultSet rs = s.executeQuery(pq);
         int t=0;
             while(rs.next()){
@@ -41,7 +41,7 @@ public class DBOP {
             int count1 = 0;
             I.setStock(count1);
             //I.setStock(Integer.parseInt(rs.getString("Count")));
-            s.executeUpdate("UPDATE stock set Count ='"+I.getStock()+"' WHERE ID='"+I.getItemID()+"'");
+            s.executeUpdate("UPDATE item set item_quantity ='"+I.getStock()+"' WHERE item_id='"+I.getItemID()+"'");
             //s.executeUpdate("UPDATE patientdetails set Total='"+pe.getTotal()+"' WHERE p_id='"+p.getPatientId()+"' ");
         
     }catch(Exception ex){
